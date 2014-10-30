cb2cf04c86670309fe09a7e90ab54c4fff161096
kenwilcox <wilcoxk@gmail.com>
2014-10-30 08:28:07 -0600

    A different way to do things

--- AlienWalkChallenge/GameScene.m
+++ AlienWalkChallenge/GameScene.m
@@ -7,37 +7,47 @@
 //
 
 #import "GameScene.h"
-#import "Alien.h"
-
-@interface GameScene()
-@property (nonatomic) Alien *player;
-@property (nonatomic) SKNode *world;
-@end
 
 @implementation GameScene
+{
+  SKSpriteNode *_character;
+  SKAction *_walkAnimation;
+  SKAction *_climbAnimation;
+  BOOL _walking;
+}
 
 - (instancetype) initWithSize:(CGSize)size
 {
   if (!(self = [super initWithSize:size]))
     return nil;
   
-  // Setup world
-  _world = [SKNode node];
-  [self addChild:_world];
+  SKTextureAtlas *atlas = [SKTextureAtlas atlasNamed:@"Alien"];
+  _character = [SKSpriteNode spriteNodeWithTexture:[atlas textureNamed:@"alienGreen_walk1"]];
+  _character.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.5);
+  [self addChild:_character];
   
-  // Setup player
-  _player = [[Alien alloc] init];
-  _player.position = CGPointMake(self.size.width * 0.5, self.size.height * 0.5);
-  [_world addChild:_player];
+  NSArray *walkFrames = @[[atlas textureNamed:@"alienGreen_walk1"],
+                          [atlas textureNamed:@"alienGreen_walk2"]];
+  _walkAnimation = [SKAction repeatActionForever:[SKAction animateWithTextures:walkFrames timePerFrame:0.15 resize:NO restore:NO]];
+  
+  NSArray *climbFrames = @[[atlas textureNamed:@"alienGreen_climb1"],
+                           [atlas textureNamed:@"alienGreen_climb2"]];
+  _climbAnimation = [SKAction repeatActionForever:[SKAction animateWithTextures:climbFrames timePerFrame:0.15 resize:NO restore:NO]];
+  
+  [_character runAction:_walkAnimation];
+  _walking = YES;
   
   return self;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-  //  for (UITouch *touch in touches) {
-  [self.player setIsWalking:!self.player.isWalking];
-  //  }
+  _walking = !_walking;
+  if (_walking) {
+    [_character runAction:_walkAnimation];
+  } else {
+    [_character runAction:_climbAnimation];
+  }
 }
 
 @end
