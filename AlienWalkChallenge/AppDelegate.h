7cd0bec7015e200eaf52b754ff48d4b2272a783e
kenwilcox <wilcoxk@gmail.com>
2014-10-30 07:08:12 -0600

    Initial Commit

---
+++ AlienWalkChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlienWalkChallenge
+//
+//  Created by Kenneth Wilcox on 10/30/14.
+//  Copyright (c) 2014 Kenneth Wilcox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
